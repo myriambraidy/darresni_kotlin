17d3cdfcc95a05b7e551c91a9f0f521f37a771fe
Jennifer Farah <jenniferfarah2003@gmail.com>
2025-04-21 18:46:30 +0300

    ft: added profile / settings navigation

--- app/src/main/java/com/myriam/projetfinal/navbar/ProfileNav.kt
+++ app/src/main/java/com/myriam/projetfinal/navbar/ProfileNav.kt
@@ -1,2 +1,25 @@
-package com.myriam.projetfinal.navbar
+package com.myriam.projetfinal.screens.ProfileScreen
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.myriam.projetfinal.Settings.SettingsScreen
+
+@Composable
+fun ProfileNav() {
+    val profileNavController = rememberNavController()
+
+    NavHost(
+        navController = profileNavController,
+        startDestination = "profileMain"
+    ) {
+        composable("profileMain") {
+            ProfileScreen(navController = profileNavController)
+        }
+        composable("settings") {
+            SettingsScreen(navController = profileNavController)
+        }
+    }
+}
