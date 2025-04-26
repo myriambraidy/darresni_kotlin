56f08c60db50d40bd544be2731ed4513f3a8e6d8
Jennifer Farah <jenniferfarah2003@gmail.com>
2025-04-26 16:27:21 +0300

    fx: profile screen layout

--- app/src/main/java/com/myriam/projetfinal/screens/ProfileScreen/ProfileScreen.kt
+++ app/src/main/java/com/myriam/projetfinal/screens/ProfileScreen/ProfileScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -46,7 +45,7 @@ fun ProfileScreen(navController: NavController) {
         }
 
         item {
-            AchievementsSectionProfile(achievements = vm.achievements)
+            AchievementsSectionProfile(navController = navController)
             Spacer(modifier = Modifier.height(100.dp))
         }
     }
