bbbb9201dee4b5f04a6b337ea7ecf0e6c7e91b9f
Jennifer Farah <jenniferfarah2003@gmail.com>
2025-04-27 22:38:50 +0300

    fx: user state flow

--- app/src/main/java/com/myriam/projetfinal/data/repositories/interfaces/UserRepository.kt
+++ app/src/main/java/com/myriam/projetfinal/data/repositories/interfaces/UserRepository.kt
@@ -1,7 +1,14 @@
 package com.myriam.projetfinal.data.repositories.interfaces
 
 import com.myriam.projetfinal.data.models.User
+import kotlinx.coroutines.flow.StateFlow // Import StateFlow
 
 interface UserRepository {
-    fun getUser(): User
+    val currentUser: StateFlow<User?>
+
+    suspend fun login(email: String, pass: String): Boolean
+
+    fun logout()
+
+
 }
